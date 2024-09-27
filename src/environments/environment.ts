f8afc46bf98178f647075056e41ba23a87a6ee13
cellmaniadesarrollo <christians9210023.tcm@gmail.com>
2024-09-27 09:45:18 -0500

    fb carrucell adaptado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
   production: false,
   apiUrl: 'https://localhost:3050',
   webSocket:'wss://localhost:3050',
-  facebook_token:'EAAVtYZCepZB6QBO4v3KcFcgl3X57l2FTqQvswPhF7LKCQKvUZAsFEjnb3b8UHmq5BTXroch5YUa94oDPrer6CLqcWz2NgWKSmATpIQZCfzYZBIElNhMsIgitsBwZAafvPjzUa98Bd4via1Qtdu4ZB9a7Ln2vcgvkG8OlJKP53GAok7TLQ30PAfX2B4tjZCQqSbuaqjdXXaSEKxhYysfGRtAZD'
+  facebook_token:'EAAVtYZCepZB6QBOZC7oJ2lmJvZAh8DFfJY0ZBEuYSwuokdbcSSPd9wPIPbZAen2wZC8eKrqHHDrwC5mhkZBhJ9ZCnn2L2ZB9rxhqePdAEoCCTfjaDJZChrjUT5mJki4onIdssrZCBrZA3jnIWacQaPYWo5LqrZBzsjmANejVnc6lHtI8xpcbLZBxiUVMMfbeGKgAhU2jxDomJtlAvuzZAomf2KutGQZDZD'
   };
