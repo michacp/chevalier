985a3ecd09347d24d296cbb30763ff9e0b145ce7
cellmaniadesarrollo <christians9210023.tcm@gmail.com>
2024-09-27 12:34:12 -0500

    distorcion en las imagenes de facebook

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
   production: false,
   apiUrl: 'https://localhost:3050',
   webSocket:'wss://localhost:3050',
-  facebook_token:'EAAVtYZCepZB6QBOZC7oJ2lmJvZAh8DFfJY0ZBEuYSwuokdbcSSPd9wPIPbZAen2wZC8eKrqHHDrwC5mhkZBhJ9ZCnn2L2ZB9rxhqePdAEoCCTfjaDJZChrjUT5mJki4onIdssrZCBrZA3jnIWacQaPYWo5LqrZBzsjmANejVnc6lHtI8xpcbLZBxiUVMMfbeGKgAhU2jxDomJtlAvuzZAomf2KutGQZDZD'
+  facebook_token:'EAAVtYZCepZB6QBO7mj2OJKf8qMIOi1yNSDpjuZAcYYZBELZCiygl1ZAK6AWDzVurnTnTZCYWUVdeEyIt234ndzyTZBfgxDsmbXWyJmB6O4SPJTFyzey5cLfkbXjNZBbNUGqIpkkpxZAPFkib1XsnSH9CIM8v0XK2WBaB7goajoA8ZBvpNske3ZAkNO22KDtQ6AOjBPFi8Lzsi3Cn18rInEZAplwZDZD'
   };
