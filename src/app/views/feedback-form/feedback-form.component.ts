b77a9a73e93d6897df4f1cbe473706b77d4e0856
cellmaniadesarrollo <christians9210023.tcm@gmail.com>
2024-10-02 13:40:16 -0500

    banner de comentarios condicional

--- src/app/views/feedback-form/feedback-form.component.ts
+++ src/app/views/feedback-form/feedback-form.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-feedback-form',
   templateUrl: './feedback-form.component.html',
   styleUrl: './feedback-form.component.css'
 })
 export class FeedbackFormComponent {
+  constructor(private router: Router) {}
 
+  isCommentsPage(): boolean {
+    return this.router.url === '/comments';
+  }
 }
