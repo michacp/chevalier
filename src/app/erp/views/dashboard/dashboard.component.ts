91e43901679cb9f67e123487b76634ff44cfbd12
cellmaniadesarrollo <christians9210023.tcm@gmail.com>
2024-11-29 15:45:07 -0500

    datos por barbero grafica

--- src/app/erp/views/dashboard/dashboard.component.ts
+++ src/app/erp/views/dashboard/dashboard.component.ts
@@ -13,6 +13,7 @@ interface Grafico {
 export class DashboardComponent {
   private semanasales: Grafico[] = [];
   private semanaanterior: Grafico[] = [];
+  transformedData: { name: string; data: { x: string; y: number }[] }[] = [];
   isDataLoaded: boolean = false; // Variable para controlar la carga de datos
 
   constructor(private authService: AuthService, private salesService: SalesService) {}
@@ -32,12 +33,27 @@ export class DashboardComponent {
       const response = await this.salesService.Salesgetdatagraph();
       this.semanasales = response.thisWeek;
       this.semanaanterior = response.lastWeek;
+      this.transformedData = this.transformData(response.thisWeekpeerbarber);
       this.isDataLoaded = true; // Marca los datos como cargados
     } catch (error) {
       console.error('Error al obtener datos:', error);
     }
   }
+  transformData(rawData: any[]): { name: string; data: { x: string; y: number }[] }[] {
+    return rawData.map((item) => ({
+      name: item.name,
+      data: item.data
+    }));
+  }
 
+  getRandomColor(): string {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
   // Métodos getter para proporcionar los datos actualizados
   getSemanaSalesData(): Grafico[] {
     return this.semanasales.length > 0 ? this.semanasales : [{ x: 'Sin datos', y: 0 }];
